a00e2f1feee6ae45b45ff7ce854111d38a27c6f3
Absolute Stratos <absolutestratos@gmail.com>
2025-08-02 16:58:05 -0700

    Fix for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://absolutestratos.github.io/",
-	base: "nobbers",
+	site: "https://nobbers.tv/",
+	// base: "nobbers",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
